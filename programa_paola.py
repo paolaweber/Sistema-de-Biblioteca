f15c36b01b5a42bdaea7df80ed9f798e50c3ecbe
Paola Weber <p.web77777@gmail.com>
2026-06-21 20:19:02 -0300

    criação do cabeçalho e criação da estrutura inicial do projeto, a classe do livro

---
+++ programa_paola.py
@@ -0,0 +1,19 @@
+#TRABALHO FINAL - ALGORÍTMOS E PROGRAMAÇÃO 2026.1 
+#Discente: Paola Weber Turma:02.
+
+#SISTEMA DE CONTROLE DE BIBLIOTECAS COM A STRUCT: CLASS
+
+class Livro:   #aqui vou deixar os dados necessários p/livros
+	
+	titulo=''
+	autor=''
+	ano=0
+	codigo=''
+	status=''
+	
+	
+	
+
+
+
+
